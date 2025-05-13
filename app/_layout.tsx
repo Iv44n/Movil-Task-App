b8b564a76f63bd550499cfef61f7b8f33c010e0c
Iv44n <ivandev2oo6@gmail.com>
2025-05-12 23:43:40 -0500

    Remove App.tsx and index.ts, add Layout and Index as new components

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Slot } from 'expo-router'
+import { StyleSheet, View } from 'react-native'
+
+export default function Layout() {
+  return (
+    <View style={styles.container}>
+      <Slot/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
