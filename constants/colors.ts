066d6cbe0690ba6c9e908653d856a59e45cdd8fd
Iv44n <ivandev2oo6@gmail.com>
2025-05-29 22:25:53 -0500

    Add color constants file with values for the app theme

---
+++ constants/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  background: '#1A1A1A',
+  card: '#242424',
+  textPrimary: '#fcfcfc',
+  textSecondary: '#B3B3B3',
+  textBlack: '#000',
+  green: '#b3fedc',
+  yellow: '#Fdf485',
+  border: '#333333'
+}
