2741a366353b4874702c8e9fd6494da9f376442e
Iv44n <ivandev2oo6@gmail.com>
2025-06-03 09:35:00 -0500

    Refactor getDb function to simplify instance check and maintain error handling

--- database/Connection.ts
+++ database/Connection.ts
@@ -4,13 +4,15 @@ let dbInstance: Sqlite.SQLiteDatabase | null = null
 const dbName = 'myDatabase.db'
 
 export async function getDb() {
-  if (dbInstance) {
-    return dbInstance
+  if (dbInstance) return dbInstance
+
+  try {
+    dbInstance = await Sqlite.openDatabaseAsync(dbName)
+    await dbInstance.execAsync('PRAGMA foreign_keys = ON;')
+    await dbInstance.execAsync('PRAGMA journal_mode = WAL;')
+
+  } catch (error) {
+    throw new Error(`Failed to open database: ${error}`)
   }
-
-  dbInstance = await Sqlite.openDatabaseAsync(dbName)
-  await dbInstance.execAsync('PRAGMA foreign_keys = ON;')
-  await dbInstance.execAsync('PRAGMA journal_mode = WAL;')
-
   return dbInstance
 }
