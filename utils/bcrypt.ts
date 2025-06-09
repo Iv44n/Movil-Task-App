cdeb871a288b0685e6032e59ff0150c98d91c99b
Iv44n <ivandev2oo6@gmail.com>
2025-06-09 15:02:26 -0500

    Remove user and project services and queries, and hash and secure storage functions

--- utils/bcrypt.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcryptjs'
-
-const SALT_ROUNDS = 10
-
-export const hashValue = async (value: string) => await bcrypt.hash(value, SALT_ROUNDS)
-
-export const compareValue = async (value: string, hashedValue: string) => await bcrypt.compare(value, hashedValue).catch(() => false)
