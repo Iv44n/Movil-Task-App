1516879aaea5bc952c5c3895adcfe7e3063361b1
Iv44n <ivandev2oo6@gmail.com>
2025-06-14 22:31:34 -0500

    refactor: replace old HomeHeader component with a new implementation using Typo for text and updated styles

---
+++ src/screens/Home/components/Header.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, View } from 'react-native'
+import useBoundStore from '@/store/useBoundStore'
+import { Colors, Sizes } from '@/constants/theme'
+import Typo from '@/components/Typo'
+
+export default function HomeHeader() {
+  const user = useBoundStore((state) => state.user)
+
+  return (
+    <View style={styles.header}>
+      <View>
+        <Typo
+          size={15}
+          color={Colors.textSecondary}
+          style={{
+            marginBottom: Sizes.spacing.s7
+          }}
+        >
+          Hi, {user?.username}!
+        </Typo>
+        <Typo size={27}>You Have</Typo>
+        <Typo
+          size={29}
+          fontWeight='semiBold'
+          style={{ textDecorationLine: 'underline' }}
+        >
+          4 Projects
+        </Typo>
+      </View>
+      {/*       <View style={styles.iconContainer}>
+        <Ionicons
+          name='mail-outline'
+          size={24}
+          color={Colors.textPrimary}
+        />
+      </View> */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: Sizes.spacing.s21
+  }
+})
