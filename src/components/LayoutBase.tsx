cc17bf5a0fe2f2ba342d8e48a9187f36b48088a5
Iv44n <ivandev2oo6@gmail.com>
2025-06-18 17:13:52 -0500

    refactor: adjust padding in LayoutBase for improved layout consistency

--- src/components/LayoutBase.tsx
+++ src/components/LayoutBase.tsx
@@ -4,7 +4,14 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function LayoutBase( { children }: React.PropsWithChildren) {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background, paddingTop: Sizes.spacing.s9 }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: Colors.background,
+        paddingHorizontal: Sizes.spacing.s9,
+        paddingTop: Sizes.spacing.s9
+      }}
+    >
       <StatusBar style='inverted'/>
       {children}
     </SafeAreaView>
