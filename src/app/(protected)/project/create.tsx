750de8eb99bcbdfee60e46e69aa38ce7d5a72e08
Iv44n <ivandev2oo6@gmail.com>
2025-06-22 15:18:34 -0500

    refactor: create CreateNewProject component to handle project creation

---
+++ src/app/(protected)/project/create.tsx
@@ -0,0 +1,10 @@
+import ScreenWrapper from '@/components/ScreenWrapper'
+import ProjectCreate from '@/screens/Project/create'
+
+export default function CreateNewProject() {
+  return (
+    <ScreenWrapper isScrollable>
+      <ProjectCreate />
+    </ScreenWrapper>
+  )
+}
