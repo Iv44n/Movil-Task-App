12bbb1d228bc3c11017c603920b8439eeef6ec2d
Iv44n <ivandev2oo6@gmail.com>
2025-06-24 17:15:54 -0500

    feat(icons): add CheckIcon component for reusable checkmark icon
    Implement a new SVG-based CheckIcon component that can be used throughout the application with customizable color and size properties.

---
+++ src/components/icons/CheckIcon.tsx
@@ -0,0 +1,22 @@
+import Svg, { G, Path } from 'react-native-svg'
+import { IconProps } from '.'
+import { Colors } from '@/constants/theme'
+
+export default function CheckIcon({
+  color = Colors.textPrimary,
+  size = 25
+}: IconProps) {
+  return (
+    <Svg
+      width={size}
+      height={size}
+      fill='none'
+      viewBox='.5 .3 23 23'
+    >
+      <G fill={color}>
+        <Path d='M16.03 10.03a.75.75 0 10-1.06-1.06l-4.47 4.47-1.47-1.47a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l5-5z' />
+
+      </G>
+    </Svg>
+  )
+}
