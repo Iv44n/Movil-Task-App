84421ac59cf3d0e4cfd622d661e1c37a900312a1
Iv44n <ivandev2oo6@gmail.com>
2025-06-29 22:07:31 -0500

    feat(app): add initial loading screen with font loading and routing logic
    Implement the main entry screen with animated loading indicator, font loading, and routing logic based on welcome status and database initialization. The screen handles navigation to either protected routes or welcome screen after successful setup.

---
+++ src/app/index.tsx
@@ -0,0 +1,60 @@
+// screens/Index.tsx
+import Typo from '@/components/Typo'
+import { useRouter } from 'expo-router'
+import { useEffect } from 'react'
+import { StyleSheet, View } from 'react-native'
+import Animated, { FadeInDown } from 'react-native-reanimated'
+import { useDrizzleDbContext } from '@/contexts/DrizzleDbContext'
+import { useFonts } from 'expo-font'
+import { Colors, Typography } from '@/constants/theme'
+import { useWelcomeDone } from '@/hooks/useWelcomeDone'
+
+export default function Index() {
+  const [fontsLoaded] = useFonts({
+    [Typography.fontFamily.extraLight]: require('../../assets/fonts/Manrope-ExtraLight.ttf'),
+    [Typography.fontFamily.light]: require('../../assets/fonts/Manrope-Light.ttf'),
+    [Typography.fontFamily.regular]: require('../../assets/fonts/Manrope-Regular.ttf'),
+    [Typography.fontFamily.medium]: require('../../assets/fonts/Manrope-Medium.ttf'),
+    [Typography.fontFamily.semiBold]: require('../../assets/fonts/Manrope-SemiBold.ttf'),
+    [Typography.fontFamily.bold]: require('../../assets/fonts/Manrope-Bold.ttf'),
+    [Typography.fontFamily.extraBold]: require('../../assets/fonts/Manrope-ExtraBold.ttf')
+  })
+
+  const router = useRouter()
+  const { success } = useDrizzleDbContext()
+  const { welcomeDone, isLoading } = useWelcomeDone()
+
+  useEffect(() => {
+    if (success === undefined || !fontsLoaded || isLoading || welcomeDone === null) return
+
+    console.log('✅ All migrations completed successfully.')
+
+    const t = setTimeout(() => {
+      const route = success && welcomeDone ? '(protected)' : 'welcome'
+      router.replace(route, { withAnchor: true })
+    }, 1200)
+
+    return () => clearTimeout(t)
+  }, [router, success, fontsLoaded, welcomeDone, isLoading])
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        entering={FadeInDown.duration(1200)}
+      >
+        <Typo size={25} fontWeight='bold'>
+          Loading...
+        </Typo>
+      </Animated.View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
