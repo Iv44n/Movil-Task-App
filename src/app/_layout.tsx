a103f736d2addd551af2619e3affac1edc1341ec
Iv44n <ivandev2oo6@gmail.com>
2025-07-07 14:17:03 -0500

    refactor(layout): replace database and session providers with auth context
    Simplify the layout component by removing SQLite and Drizzle providers in favor of a single AuthContextProvider. This change aligns with the new authentication strategy.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,37 +1,25 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import LayoutBase from '@/components/LayoutBase'
-import { SQLiteProvider } from 'expo-sqlite'
-import { DrizzleDbProvider } from '@/contexts/DrizzleDbContext'
 import { Stack } from 'expo-router'
-import SessionContextProvider from '@/contexts/SessionContext'
+import { AuthContextProvider } from '@/context/AuthContext'
 
 export default function Layout() {
   return (
     <GestureHandlerRootView>
       <LayoutBase>
-        <SQLiteProvider
-          databaseName='task-test'
-          options={{
-            enableChangeListener: true,
-            useNewConnection: true
-          }}
-        >
-          <SessionContextProvider>
-            <DrizzleDbProvider>
-              <Stack
-                screenOptions={{
-                  headerShown: false,
-                  contentStyle: { backgroundColor: 'transparent' }
-                }}
-              >
-                <Stack.Screen name='index' options={{ animation: 'fade', animationDuration: 1000 }}/>
-                <Stack.Screen name='welcome' options={{ animation: 'fade' }}/>
-                <Stack.Screen name='(auth)' options={{ animation: 'fade' }}/>
-                <Stack.Screen name='(protected)' options={{ animation: 'fade' }} />
-              </Stack>
-            </DrizzleDbProvider>
-          </SessionContextProvider>
-        </SQLiteProvider>
+        <AuthContextProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: { backgroundColor: 'transparent' }
+            }}
+          >
+            <Stack.Screen name='index' options={{ animation: 'fade', animationDuration: 1000 }}/>
+            <Stack.Screen name='welcome' options={{ animation: 'fade' }}/>
+            <Stack.Screen name='(auth)' options={{ animation: 'fade' }}/>
+            <Stack.Screen name='(protected)' options={{ animation: 'fade' }} />
+          </Stack>
+        </AuthContextProvider>
       </LayoutBase>
     </GestureHandlerRootView>
   )
