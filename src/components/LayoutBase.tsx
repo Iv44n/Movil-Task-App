013857e5123988cb7040960195fcc48b530c1454
Iv44n <ivandev2oo6@gmail.com>
2025-07-08 11:59:19 -0500

    refactor(LayoutBase): remove top padding and adjust status bar style
    Remove redundant top padding and change status bar style from 'inverted' to 'light' for better consistency with the app's design

--- src/components/LayoutBase.tsx
+++ src/components/LayoutBase.tsx
@@ -2,17 +2,16 @@ import { Colors, Sizes } from '@/constants/theme'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function LayoutBase( { children }: React.PropsWithChildren) {
+export default function LayoutBase({ children }: React.PropsWithChildren) {
   return (
     <SafeAreaView
       style={{
         flex: 1,
         backgroundColor: Colors.background,
-        paddingHorizontal: Sizes.spacing.s9,
-        paddingTop: Sizes.spacing.s9
+        paddingHorizontal: Sizes.spacing.s9
       }}
     >
-      <StatusBar style='inverted'/>
+      <StatusBar style='light'/>
       {children}
     </SafeAreaView>
   )
