feb437774a0c4b13744b1012f84c7249e4f3e752
Iv44n <ivandev2oo6@gmail.com>
2025-07-11 10:37:07 -0500

    refactor(deps): update expo and remove unused dependencies
    Remove react-native-gesture-handler and related wrapper as it's no longer needed
    Clean up package.json by removing unused dependencies and organizing imports

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,23 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import LayoutBase from '@/components/LayoutBase'
 import { Stack } from 'expo-router'
 import { AuthContextProvider } from '@/context/AuthContext'
 
 export default function Layout() {
   return (
-    <GestureHandlerRootView>
-      <LayoutBase>
-        <AuthContextProvider>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: { backgroundColor: 'transparent' }
-            }}
-          >
-            <Stack.Screen name='index' options={{ animation: 'fade', animationDuration: 1000 }}/>
-            <Stack.Screen name='welcome' options={{ animation: 'fade' }}/>
-            <Stack.Screen name='(auth)' options={{ animation: 'fade' }}/>
-            <Stack.Screen name='(protected)' options={{ animation: 'fade' }} />
-          </Stack>
-        </AuthContextProvider>
-      </LayoutBase>
-    </GestureHandlerRootView>
+    <LayoutBase>
+      <AuthContextProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: 'transparent' }
+          }}
+        >
+          <Stack.Screen name='index' options={{ animation: 'fade', animationDuration: 1000 }}/>
+          <Stack.Screen name='welcome' options={{ animation: 'fade' }}/>
+          <Stack.Screen name='(auth)' options={{ animation: 'fade' }}/>
+          <Stack.Screen name='(protected)' options={{ animation: 'fade' }} />
+        </Stack>
+      </AuthContextProvider>
+    </LayoutBase>
   )
 }
