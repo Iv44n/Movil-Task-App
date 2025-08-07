fd39dde9a3e6964bf11adc06d05917c7fd4cb1a3
Iv44n <ivandev2oo6@gmail.com>
2025-08-06 23:40:03 -0500

    refactor(types): update project and project task type definitions for form handling
    - Removed unnecessary fields from InsertProjectForForm and InsertProjectTaskForForm types to streamline form data handling.
    - Adjusted UpdateProjectForForm type to exclude 'task_count' for better clarity in project updates.

--- src/types/Project.d.ts
+++ src/types/Project.d.ts
@@ -6,4 +6,4 @@ export type UpdateProject = Database['public']['Tables']['projects']['Update']
 
 // Omitting fields that are not needed for the form
 export type InsertProjectForForm = Omit<InsertProject, 'id' | 'created_at' | 'updated_at' | 'task_count' | 'deleted'>
-export type UpdateProjectForForm = Omit<UpdateProject, 'user_id' | 'id' | 'created_at' | 'updated_at' | 'task_count' | 'deleted'>
+export type UpdateProjectForForm = Omit<UpdateProject, 'user_id' | 'id' | 'created_at' | 'updated_at' | 'deleted'>
