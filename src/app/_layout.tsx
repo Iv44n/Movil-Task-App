9652f508304b516481d23139285d582791e1e124
Iv44n <ivandev2oo6@gmail.com>
2025-08-10 17:49:53 -0500

    feat(i18n): initialize language settings on app load
    - Implemented language initialization in the layout component to set the app's locale based on stored preferences or device settings.
    - Utilized AsyncStorage to retrieve the user's preferred language, with a fallback to the device's default language if no preference is found.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,26 @@
 import LayoutBase from '@/components/LayoutBase'
 import { Stack } from 'expo-router'
 import { AuthContextProvider } from '@/context/AuthContext'
+import i18n from '@/i18n'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { getLocales } from 'expo-localization'
+import { useEffect } from 'react'
+import { LANG_STORAGE_KEY } from '@/constants/constants'
 
 export default function Layout() {
+  useEffect(() => {
+    const initLanguage = async () => {
+      try {
+        const storedLang = await AsyncStorage.getItem(LANG_STORAGE_KEY)
+        i18n.locale = storedLang ?? getLocales()[0]?.languageCode ?? 'en'
+      } catch (error) {
+        console.error('Error loading language', error)
+        i18n.locale = getLocales()[0]?.languageCode ?? 'en'
+      }
+    }
+    initLanguage()
+  }, [])
+
   return (
     <LayoutBase>
       <AuthContextProvider>
